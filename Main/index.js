7af98f224b601934fb60773bbd80c2ef87610a37
edirie <essedirie@gmail.com>
2022-02-12 17:02:22 -0500

    creating inquirer functions with prompt

--- Main/index.js
+++ Main/index.js
@@ -12,6 +12,69 @@ app.use(express.json());
 
 db.connect((err) => {
     if (err) throw err;
-    start()
+    start();
 });
 
+function start (){
+    inquirer.prompt([
+        {
+          type: 'list',
+          message: 'What would you like to do?',
+          name: 'optionsList',
+          choices: ["View all Employees", "View All Roles", "View All Departments", "View employee by Manager", "Add a new department", "Add a new role", "Add a new Employee", "Update an employee role", "Delete a row (Department/Role/Employee)", "Quit"],
+          name:"choice" //cleans up the output
+        }
+
+      ])        
+//         .then((res) => {
+//           console.log(res.choice); //shows output of res
+
+//           switch(res.choice){
+
+//             case "View all Employees":
+//             employeeView();
+//             break;
+
+//             case "View All Roles":
+//             roleView()
+//             break;
+
+//             case "View All Departments":
+//             departmentView()
+//             break;
+
+//             case "View employee by Manager":
+//             managerView()
+//             break;
+
+
+
+//             case "Add a new department":
+//             addDepartment()
+//             break;
+
+//             case "Add a new role":
+//             addRole()
+//             break;
+
+//             case "Add a new Employee":
+//             addEmployee()
+//             break;
+
+//             case "Update an employee role":
+//             updateEmployee()
+//             break;
+
+
+//             case "Delete a row (Department/Role/Employee)":
+//             deleteaRow()
+//             break;
+
+
+//             case "Quit":
+//             db.end();
+//             console.log("Bye!")
+//             break;
+//           }
+
+          }
